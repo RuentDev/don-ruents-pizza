3033afdeb77785cca6cc160ad3e4dbbf399c0d38
CRuentgen <ruent.dev@gmail.com>
2023-11-02 20:58:29 +0800

    added

--- src/components/Input/FilterInput.tsx
+++ src/components/Input/FilterInput.tsx
@@ -5,14 +5,14 @@ const FilterInput = () => {
     <div className='range-input w-full h-auto'>
       <div className="range-contaier relative w-full h-auto">
         <input 
-          className='absolute'
+          className='absolute '
           type="range" 
           min={0} 
           max="100" 
           step="100" 
         />
         <input 
-          className='absolute'
+          className='absolute '
           type="range" 
           min={0} 
           max="100" 
