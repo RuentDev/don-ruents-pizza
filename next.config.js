e61077d1ad6bb193e19c359effdf3db77e6f4fd7
CRuentgen <ruent.dev@gmail.com>
2024-08-02 11:45:52 +0800

    added shopify storefront api

--- next.config.js
+++ next.config.js
@@ -2,6 +2,20 @@
 const nextConfig = {
     reactStrictMode: false,
     images: { unoptimized: true },
+    env: {
+        SHOPIFY_STORE_DOMAIN: process.env.SHOPIFY_STORE_DOMAIN,
+        SHOPIFY_STOREFRONT_ACCESS_TOKEN: process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN,
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'cdn.shopify.com',
+                port: '',
+                pathname: '/s/files/**',
+            },
+        ],
+    }
 }
 
 module.exports = nextConfig
---
+++ src/app/api/products/products.tsx
@@ -0,0 +1,46 @@
+import client from "@/utils/shopify";
+
+export default async function products() {
+  const productQuery = `{
+    products (first: 10) {
+      nodes {
+        id
+        handle
+        title
+        tags
+        description
+        priceRange {
+          minVariantPrice {
+            amount
+            currencyCode
+          }
+        }
+        featuredImage {
+          url
+        }
+        productType
+        totalInventory
+        availableForSale
+        vendor
+        createdAt
+        updatedAt
+        publishedAt
+        onlineStoreUrl
+        options {
+          id
+          name
+          values
+        }
+      }
+      pageInfo {
+        hasNextPage
+        endCursor
+      }
+    }
+  }`;
+
+
+  const { data } = await client.request(productQuery)
+
+  return data.products.nodes
+}
