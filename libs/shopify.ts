782e74ecc7bc4f3a1854037d85916939d1f05962
RuentDev <ruent.dev@gmail.com>
2026-08-18 23:33:16 +0800

    fix: update Shopify storefront access token environment variable prefix

--- libs/shopify.ts
+++ libs/shopify.ts
@@ -3,7 +3,8 @@ import { createStorefrontApiClient } from "@shopify/storefront-api-client";
 const client = createStorefrontApiClient({
   storeDomain: process.env.NEXT_PUBLIC_SHOPIFY_STORE_DOMAIN as string,
   apiVersion: process.env.NEXT_PUBLIC_SHOPIFY_STORE_API_VERSION as string,
-  publicAccessToken: process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN as string,
+  publicAccessToken: process.env
+    .NEXT_PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN as string,
 });
 
 export default client;
